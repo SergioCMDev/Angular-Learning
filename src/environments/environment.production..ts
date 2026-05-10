35bf2d099fd47090f4a22e4ec60d9376b6897e3a
SergioCMDev <sergiocmdev@gmail.com>
2026-05-10 21:07:03 +0200

    Añadido environments

---
+++ src/environments/environment.production..ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    tmdbApiKey: 'b33741a15982cc88910d1de05faab5c2',
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    tmdbApiKey: 'b33741a15982cc88910d1de05faab5c2',
+}
